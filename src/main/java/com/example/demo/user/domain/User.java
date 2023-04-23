d0d8728b770a07338f62ec81ea6ca1e62666a78b
Leesungsup <tmdtjq0116@naver.com>
2023-04-23 12:07:11 +0900

    CorsConfig 추가와 User 컬럼(id->Usercode로 수정)

--- src/main/java/com/example/demo/user/domain/User.java
+++ src/main/java/com/example/demo/user/domain/User.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 @Table(name = "user_master")
 public class User {
 
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userCode;
     private Long kakaoId;
