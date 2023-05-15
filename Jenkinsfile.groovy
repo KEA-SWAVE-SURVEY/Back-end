b4139531d10a782a9bd453c3bb7a93d6ebfa3851
Seong-guk Kim <54500840+kjk7212@users.noreply.github.com>
2023-05-15 12:16:01 +0900

    cicd

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    environment {
+        TARGET_HOST = "root@172.16.210.24"
+    }
+    
+    stages() {
+        stage('git clone') {
+            steps() {
+                git branch: 'main', credentialsId: 'git-kjk7212', url: 'https://github.com/KEA-SWAVE-SURVEY/Back-end/'
+            }
+        }
+        
+        stage('build') {
+            steps {
+                 sh "./gradlew clean build"
+            }
+        }
+        
+        stage('build & push docker image') {
+            steps {
+                script{
+                    image = docker.build("kjk7212/back")
+                    docker.withRegistry('kjk7212/back', 'docker-hub-credentials') {
+                        image.push()
+                    }
+                }
+            }
+        }
+        stage('ws deployment') {
+            steps{
+                sshagent(['WS']) {
+                sh "ssh -o StrictHostKeyChecking=no ${TARGET_HOST} 'docker stop back'"
+                sh "ssh -o StrictHostKeyChecking=no ${TARGET_HOST} 'docker rm back'"
+                sh "ssh -o StrictHostKeyChecking=no ${TARGET_HOST} 'docker rmi kjk7212/back'"
+                sh "ssh -o StrictHostKeyChecking=no ${TARGET_HOST} 'docker pull kjk7212/back'"
+                sh "ssh -o StrictHostKeyChecking=no ${TARGET_HOST} 'docker run --name back -d -p 8080:8080 kjk7212/back'"
+                }
+            }
+        }
+    }
+}
